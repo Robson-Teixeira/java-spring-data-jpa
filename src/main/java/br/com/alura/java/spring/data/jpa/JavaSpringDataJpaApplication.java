45d3c03bad22641667926e82656df924d2cabf46
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-12-26 00:00:27 -0300

    Primeiro Repositório: Inserindo valores

--- src/main/java/br/com/alura/java/spring/data/jpa/JavaSpringDataJpaApplication.java
+++ src/main/java/br/com/alura/java/spring/data/jpa/JavaSpringDataJpaApplication.java
@@ -1,13 +1,31 @@
 package br.com.alura.java.spring.data.jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.alura.java.spring.data.jpa.orm.Cargo;
+import br.com.alura.java.spring.data.jpa.repository.CargoRepository;
+
 @SpringBootApplication
-public class JavaSpringDataJpaApplication {
+public class JavaSpringDataJpaApplication implements CommandLineRunner {
+
+	private final CargoRepository cargoRepository;
+
+	public JavaSpringDataJpaApplication(CargoRepository cargoRepository) {
+		this.cargoRepository = cargoRepository;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaSpringDataJpaApplication.class, args);
+	}
+
+	@Override
+	// Método da CommandLineRunner executado após o método main
+	public void run(String... args) throws Exception {
+		Cargo cargo = new Cargo();
+		cargo.setDescricao("Desenvolvedor de Software");
+		cargoRepository.save(cargo);
 	}
 
 }
--- src/main/java/br/com/alura/java/spring/data/jpa/repository/CargoRepository.java
+++ src/main/java/br/com/alura/java/spring/data/jpa/repository/CargoRepository.java
@@ -3,7 +3,9 @@ package br.com.alura.java.spring.data.jpa.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import br.com.alura.java.spring.data.jpa.orm.Cargo;
+
 @Repository
-public interface CargoRepository extends CrudRepository {
+public interface CargoRepository extends CrudRepository<Cargo, Integer> {
 
 }
