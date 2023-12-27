d9384048e073e0012dd1e3835774e5b9a9182cf3
Robson-Teixeira <robsonmteixeira@outlook.com>
2023-12-27 18:47:17 -0300

    Derived Queries, JPQL e Native Queries: Usando JPQL

--- src/main/java/br/com/alura/java/spring/data/jpa/control/employees/repository/FuncionarioRepository.java
+++ src/main/java/br/com/alura/java/spring/data/jpa/control/employees/repository/FuncionarioRepository.java
@@ -1,7 +1,9 @@
 package br.com.alura.java.spring.data.jpa.control.employees.repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,9 @@ import br.com.alura.java.spring.data.jpa.control.employees.orm.Funcionario;
 @Repository
 public interface FuncionarioRepository extends CrudRepository<Funcionario, Integer> {
 	List<Funcionario> findByNome(String nome);
+
+	// List<Funcionario> findByNomeAndSalarioGreaterThanAndDataContratacao(String nome, Double salario, LocalDate data);
+	@Query("SELECT f FROM Funcionario f WHERE f.nome = :nome AND f.salario >= :salario AND f.datacontratacao = :datacontratacao")
+	// Especifica que não se trata de uma derived query
+	List<Funcionario> findNomeSalarioMaiorDataContratacao(String nome, Double salario, LocalDate datacontratacao);
 }
